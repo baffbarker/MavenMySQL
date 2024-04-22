18ea0af7945e56f860a89152b11476195c2bace9
Jeremy Stocks <jeremy.stocks@zoho.com>
2024-04-22 22:32:30 +0200

    Creating Test Users

--- src/test/java/com/caveofprogramming/db/mysql/UserDaoImplTest.java
+++ src/test/java/com/caveofprogramming/db/mysql/UserDaoImplTest.java
@@ -3,19 +3,55 @@ package com.caveofprogramming.db.mysql;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import com.mysql.cj.x.protobuf.MysqlxCrud.Collection;
+
 public class UserDaoImplTest {
 	
 	private Connection conn;
+	private List<User> users;
+	
+	private static final int NUM_TEST_USERS = 1000;
+	
+	private List<User> loadUsers() throws IOException {
+		
+		//@formatter:off
+		
+		return Files
+			.lines(Paths.get("../greatexpectations.txt"))
+			.map(line -> line.split("[^A-Za-z]"))
+			.map(Arrays::asList)
+			.flatMap(List -> List.stream())
+			.filter(word -> word.length() > 3 & word.length() < 20)
+			.map(word -> new User(word))
+			.limit(NUM_TEST_USERS)
+			.collect(Collectors.toList());
+		
+		//@formatter:on
+		
+	}	
+		
 
 	@Before
-	public void setUp() throws SQLException {
+	public void setUp() throws SQLException, IOException {
+		
+		users = loadUsers();
+
+		System.out.println(users);
+		System.out.println(users.size());
+		
 		var props = Profile.getProperties("db");
 		
 		var db = Database.instance();
